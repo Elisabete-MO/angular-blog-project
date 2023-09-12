c956ac970816fc6f8187ff90527545593555d73e
Bete.mrt <bete.mrt@gmail.com>
2023-09-12 01:28:50 -0300

    :lipstick: feat: cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { CardComponent } from './components/card/card.component';
+import { CardSmallComponent } from './components/card-small/card-small.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    CardComponent,
+    CardSmallComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/card-small/card-small.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-small',
+  templateUrl: './card-small.component.html',
+  styleUrls: ['./card-small.component.css']
+})
+export class CardSmallComponent {
+
+}
